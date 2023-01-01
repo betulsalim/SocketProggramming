73aac5d896554fded49e63bcd599da5b823fd1e7
betul <betulsalim6@gmail.com>
2023-01-01 19:46:26 +0300

    first commit

---
+++ src/main/java/com/example/socketproggramming/User.java
@@ -0,0 +1,22 @@
+package com.example.socketproggramming;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 4801633306273802062L;
+    public String nameString;
+    public String fullNameString;
+    public String passwordString;
+    public String emailString;
+    public String genderString;
+    public String phoneNumberString;
+
+
+    @Override
+    public String toString() {
+        Room room = new Room();
+        return nameString + " " + passwordString + " ";
+    }
+}
