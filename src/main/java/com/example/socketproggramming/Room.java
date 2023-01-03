c35dce9f26fa9fdb24594012bdc38a1865b48230
Betül Salim <112887429+betulsalim@users.noreply.github.com>
2023-01-03 17:30:10 +0300

    Update Room.java

--- src/main/java/com/example/socketproggramming/Room.java
+++ src/main/java/com/example/socketproggramming/Room.java
@@ -1,42 +1,61 @@
 package com.example.socketproggramming;
 
+import com.example.socketproggramming.HelloController.*;
 import animatefx.animation.FadeIn;
+import animatefx.animation.FadeOut;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.NodeOrientation;
 import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import com.example.socketproggramming.User;
+
+import org.apache.commons.lang3.StringUtils;
+
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.io.Serializable;
 
-import static com.example.socketproggramming.HelloController.users;
+import static com.example.socketproggramming.HelloController.*;
 
-public class Room extends Thread implements Initializable, Serializable {
+public class Room extends Thread implements Initializable,Serializable  {
     private static final long serialVersionUID = 4801633306273802062L;
     File newUserFile = new File("User.txt");
 
     @FXML
     public Label clientNameLabel;
+    @FXML
+    public TextField changePasswordTextField;
+
+    @FXML
+    public Button changePasswordButton;
 
     @FXML
     public Button chatBoxButton;
@@ -51,7 +70,7 @@ public class Room extends Thread implements Initializable, Serializable {
     public TextArea messageRoomArea;
 
     @FXML
-    public Label onlineLabel;
+    public Label onlineLabel ;
 
     @FXML
     public Label fullNameLabel;
@@ -83,7 +102,7 @@ public class Room extends Thread implements Initializable, Serializable {
 
     private FileChooser fileChooser;
     private File filePathFile;
-    public boolean toggleChat = false, toggleProfile = false;
+    public boolean toggleChat = false , toggleProfile= false;
 
 
     BufferedReader reader;
@@ -92,13 +111,13 @@ public class Room extends Thread implements Initializable, Serializable {
 
     public void connectSocket() {
         try {
-            socket = new Socket("localhost", 9090);
+            socket = new Socket("localhost",9090);
             System.out.println("Socket is connected with Server!");
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new PrintWriter(socket.getOutputStream(), true);
+            writer = new PrintWriter(socket.getOutputStream(),true);
             this.start();
 
-        } catch (IOException exception) {
+        }catch (IOException exception) {
             exception.printStackTrace();
         }
     }
@@ -106,49 +125,50 @@ public class Room extends Thread implements Initializable, Serializable {
     @Override
     public void run() {
         try {
-            while (true) {
+            while(true) {
                 String messageString = reader.readLine();
-                String[] tokensString = messageString.split(" ");
+                String[] tokensString=messageString.split(" ");
                 String cmdString = tokensString[0];
                 System.out.println(cmdString);
                 StringBuilder fullMessageBuilder = new StringBuilder();
-                for (int i = 1; i < tokensString.length; i++) {
+                for(int i=1 ; i<tokensString.length;i++) {
                     fullMessageBuilder.append(tokensString[i]);
                 }
                 System.out.println(fullMessageBuilder);
-                if (cmdString.equalsIgnoreCase(HelloController.username + ":")) {
+                if(cmdString.equalsIgnoreCase(HelloController.username+ ":")) {
                     continue;
 
-                } else if (fullMessageBuilder.toString().equalsIgnoreCase("bye")) {
+                }else if (fullMessageBuilder.toString().equalsIgnoreCase("bye")) {
                     break;
                 }
-                messageRoomArea.appendText(messageString + "\n");
+                messageRoomArea.appendText(messageString+ "\n");
             }
             reader.close();
             writer.close();
             socket.close();
-        } catch (Exception exception) {
+        }catch (Exception exception) {
             exception.printStackTrace();
         }
     }
 
 
+
     public void handleProfileButton(ActionEvent event) {
-        if (event.getSource().equals(profileButton) && !toggleProfile) {
+        if(event.getSource().equals(profileButton)  && !toggleProfile) {
             new FadeIn(profileButton).play();
             profileButton.toFront();
             chatPane.toBack();
-            toggleProfile = true;
-            toggleChat = false;
+            toggleProfile=true;
+            toggleChat=false;
             profileButton.setText("Back");
             setProfile();
 
 
-        } else if (event.getSource().equals(profileButton) && toggleProfile) {
+        }else if (event.getSource().equals(profileButton) && toggleProfile) {
             new FadeIn(chatPane).play();
             chatPane.toFront();
-            toggleProfile = false;
-            toggleChat = false;
+            toggleProfile=false;
+            toggleChat=false;
             profileButton.setText("Profile");
 
         }
@@ -157,8 +177,8 @@ public class Room extends Thread implements Initializable, Serializable {
 
 
     public void setProfile() {
-        for (User user : users) {
-            if (HelloController.username.equalsIgnoreCase(user.nameString)) {
+        for(User user: users) {
+            if(HelloController.username.equalsIgnoreCase(user.nameString)) {
                 fullNameLabel.setText(user.fullNameString);
                 fullNameLabel.setOpacity(1);
                 emaiLabel.setText(user.emailString);
@@ -175,21 +195,20 @@ public class Room extends Thread implements Initializable, Serializable {
 
     public void handeSendEvent(MouseEvent event) {
         send();
-        for (User user : users) {
+        for(User user:users) {
             System.out.println(user.nameString);
         }
 
     }
-
     public void send() {
-        String writer = "";
-        String messageString = messageField.getText();
+        String writer= "";
+        String messageString=messageField.getText();
         writer.intern();
         // writer.println(HelloController.username + ": " + messageString);
         messageRoomArea.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
-        messageRoomArea.appendText("Me: " + messageString + " \n");
+        messageRoomArea.appendText("Me: "+messageString+" \n");
         messageField.setText("");
-        if (messageString.equalsIgnoreCase("BYE") || messageString.equalsIgnoreCase("logout")) {
+        if(messageString.equalsIgnoreCase("BYE") || messageString.equalsIgnoreCase("logout")) {
             System.exit(0);
         }
     }
@@ -201,12 +220,14 @@ public class Room extends Thread implements Initializable, Serializable {
     public void chooseImageButton(ActionEvent event) {
 
 
+
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         fileChooser = new FileChooser();
         fileChooser.setTitle("Open Image");
         this.filePathFile = fileChooser.showOpenDialog(stage);
         fileChoosePathField.setText(filePathFile.getPath());
         saveControl = true;
+
 
 
     }
@@ -223,6 +244,7 @@ public class Room extends Thread implements Initializable, Serializable {
     public void saveImage() {
 
 
+
         if (saveControl) {
 
 
@@ -237,41 +259,53 @@ public class Room extends Thread implements Initializable, Serializable {
             } catch (IOException exception) {
                 System.err.println(exception.getMessage());
             }
+            File newFile = new File("ChangeImage");
+            if(newUserFile.exists()){
+            try {
+                FileWriter fileWriter = new FileWriter("ChangeImage.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                printWriter.write(filePathFile.getPath());
+
+                printWriter.close();
+
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+
 
         }
     }
 
+
     @Override
+
     public void initialize(URL location, ResourceBundle resourceBundle) {
+
+
+
         showProfilePictureCircle.setStroke(Color.valueOf("#3344"));
         Image image;
+        if (HelloController.gender.equalsIgnoreCase("Male")) {
+            image = new Image("C:\\Users\\betul\\Desktop\\JavaFx\\SocketProggramming\\src\\İmage\\man2.png", false);
 
-        if (HelloController.gender.equalsIgnoreCase("MALE")) {
-            image = new Image("C:\\Users\\betul\\Desktop\\JavaFx\\SocketProggramming\\src\\İmage\\man2.png");
+
         } else {
-            image = new Image("C:\\Users\\betul\\Desktop\\JavaFx\\SocketProggramming\\src\\İmage\\female.png");
+            image = new Image("C:\\Users\\betul\\Desktop\\JavaFx\\SocketProggramming\\src\\İmage\\female.png", false);
             profileImageView.setImage(image);
         }
         showProfilePictureCircle.setFill(new ImagePattern(image));
-        if (newUserFile.exists()) {
-
-            try {
-                FileWriter fileWriter = new FileWriter("loggedInUserFile.txt");
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                bufferedWriter.append((CharSequence) showProfilePictureCircle.getFill());
-                bufferedWriter.close();
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
 
 
             clientNameLabel.setText(HelloController.username);
             connectSocket();
-        }
+
+
     }
 
 
 }
-
 
 
